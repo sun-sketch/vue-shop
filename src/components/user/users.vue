844aae8141eac3529001bc48ee3d9ef58def2e77
sun-sketch <1462353882@qq.com>
2020-06-20 20:50:00 +0800

    修改

--- src/components/user/users.vue
+++ src/components/user/users.vue
@@ -38,7 +38,8 @@
             <el-button type="primary" icon="el-icon-edit" size="mini"
             @click="showEditDialog(slotProps.row.id)"></el-button>
             <!-- 删除按钮 -->
-            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini"
+            @click="removeUserById(slotProps.row.id)"></el-button>
             <!-- 分配角色按钮 -->
             <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
               <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
@@ -263,6 +264,24 @@ export default {
          this.getUserList();
          this.$message.success('更新用户信息成功');
        })
+    },
+    //根据id删除对应的用户信息
+    async removeUserById(id){
+      const confirmResult=await this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).catch(err=>err)
+        console.log(confirmResult);
+        if(confirmResult!=='confirm'){
+          return this.$message.info('已取消删除');
+        }
+        const{data:res}=await this.$http.delete('users/'+id);
+        if(res.meta.status!==200){
+          return this.$message.error('删除用户失败');
+        }
+        this.getUserList();
+        this.$message.success('删除用户成功');
     }
   }
 }
